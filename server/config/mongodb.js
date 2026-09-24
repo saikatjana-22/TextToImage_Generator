bc2dc661d1ceb894b309ca31e080ecbda6636b3b
Saikat Jana <saikatjana439@gmail.com>
2026-09-24 19:12:31 +0530

    update

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database Connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI}/imagify`);
+    await mongoose.connect(process.env.MONGODB_URI);
 };
 
 export default connectDB;
